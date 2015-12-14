09505eaa3fd36339a488bd646146d389bb557844
Eugen Istoc <eugenistoc@gmail.com>
2015-12-14 01:14:25 -0500

    Bugfix

--- server/github.js
+++ server/github.js
@@ -1,8 +1,12 @@
 var GitHubApi = require("github");
+var GITHUB_KEY;
+
 try {
-  var ENV = require('./environments.js');
+  GITHUB_KEY = require('./environments.js')['GITHUB_KEY'];
+
 } catch (e) {
   console.log("Local environment vars module not found, using system environment vars instead");
+  GITHUB_KEY = process.env.GITHUB_KEY;
 }
 
 var github = new GitHubApi({
@@ -12,7 +16,7 @@ var github = new GitHubApi({
 
 github.authenticate({
   type: "oauth",
-  token: ENV['GITHUB_KEY']
+  token: GITHUB_KEY
 });
 
 module.exports = github;
